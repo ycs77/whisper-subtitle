031488f8a5e94a071f842a74a209a5bff5d63649
Lucas Yang <yangchenshin77@gmail.com>
2023-03-22 13:45:38 +0800

    Tweak

--- utils.js
+++ utils.js
@@ -4,8 +4,7 @@ const spawn = require('cross-spawn')
 module.exports.getDuration = function (path) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const cmd = `ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${path}`
-      exec(cmd, (error, stdout, stderr) => {
+      exec(`ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${path}`, (error, stdout, stderr) => {
         if (error) {
           reject(error)
           return
